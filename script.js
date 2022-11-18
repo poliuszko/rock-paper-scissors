8b99c93dd08ed5c4cfac4bc4d5d433369267f7b9
poliuszko <poliuszko@gmail.com>
2022-11-18 14:56:37 +0100

    Add playRound and playGame

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const choices = ['paper', 'rock', 'scissors'];
+console.log(choices);
+
+const getComputerChoice = function() {
+
+return choices[(Math.trunc(Math.random()*3))]};
+//return choices[(Math.trunc(Math.random()*3))]};
+
+console.log(getComputerChoice());
+
+const playRound = function(playerSelection, computerSelection) {
+//    let playerSelection = prompt('Plase select: Paper Rock or Scissors');
+//    console.log(playerSelection);
+    console.log(`Player selection ${playerSelection} -- Computer Selection ${computerSelection}`);
+    if (playerSelection === 'paper' && computerSelection === 'rock' ) {
+        return [1,0] }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors' ){
+        return [1,0] }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper' ){
+        return [1,0] }
+    else if  (playerSelection === computerSelection ) { 
+        return [0,0] }
+    else {
+        return [0,1] }
+};
+
+const game = function() {
+    let gameScore = [0,0];
+    for (let i = 0; i < 5; i++) {
+        console.log(`GAME ${i+1}`)
+        roundScore=playRound(prompt('Please give your selection:').toLowerCase(),getComputerChoice());
+        gameScore[0]+= roundScore[0];
+        gameScore[1]+= roundScore[1];
+     console.log(gameScore);   
+           
+    }
+};
+
+//playRound(prompt('Please give your selection:'),getComputerChoice());
+game();
